02b0c7a8cf0d0180cc42e0e665ed16f1183b29ee
Daniel Bartl <danielbartl@Daniel-PC>
2024-10-10 23:30:47 +0200

    Add address fields and improve feedback on form
    Enhanced the PersonalInformationPage form by adding new fields for street address, city, region, and postal code. Implemented separate feedback panels to display success and error messages more clearly. Streamlined logging and replaced "log" with "LOG" for consistency.

--- src/main/java/dev/danvega/PersonalInformationPage.java
+++ src/main/java/dev/danvega/PersonalInformationPage.java
@@ -1,7 +1,11 @@
 package dev.danvega;
 
+import java.util.stream.Stream;
+
 import org.apache.wicket.bean.validation.PropertyValidator;
+import org.apache.wicket.feedback.FeedbackMessage;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.EmailTextField;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
@@ -15,7 +19,7 @@ import org.slf4j.LoggerFactory;
 
 public class PersonalInformationPage extends WebPage {
 
-	private static final Logger log = LoggerFactory.getLogger(PersonalInformationPage.class);
+	private static final Logger LOG = LoggerFactory.getLogger(PersonalInformationPage.class);
 
 	@SpringBean
 	private UserRepository repository;
@@ -31,6 +35,11 @@ public class PersonalInformationPage extends WebPage {
 		firstName();
 		lastName();
 		email();
+		country();
+		streetAddress();
+		city();
+		region();
+		postalCode();
 
 	}
 
@@ -41,33 +50,60 @@ public class PersonalInformationPage extends WebPage {
 			@Override
 			protected void onSubmit() {
 
-				log.info("Saving User: {}", getModelObject());
-				assert repository != null;
-				//model.addAttribute("message", "User information saved successfully!");
+				LOG.info("Saving User: {}", getModelObject());
+				repository.save(getModelObject());
+				success("User information saved successfully!");
 
 			}
 
 			@Override
 			protected void onError() {
 
-				log.info("Invalid User: {}", getModelObject());
+				LOG.info("User Validation failed for: {}", getModelObject());
 
 			}
 		};
 
-		FeedbackPanel feedbackPanel = new FeedbackPanel("feedback"){
-
-			@Override
-			protected void onConfigure() {
-				super.onConfigure();
-				setVisible(anyErrorMessage());
-			}
-
-		};
-		feedbackPanel.setOutputMarkupPlaceholderTag(true);
-		form.add(feedbackPanel);
-
+		success();
+		errors();
 		add(form);
+
+	}
+
+	private void errors() {
+		FeedbackPanel errors =
+				new FeedbackPanel(
+						"errors",
+						msg -> msg.getLevel() == FeedbackMessage.ERROR) {
+
+					@Override
+					protected void onConfigure() {
+						super.onConfigure();
+						setVisible(anyErrorMessage());
+					}
+
+				};
+		errors.setOutputMarkupPlaceholderTag(true);
+		form.add(errors);
+	}
+
+	private void success() {
+
+		FeedbackPanel success =
+				new FeedbackPanel(
+						"success",
+						msg -> msg.getLevel() == FeedbackMessage.SUCCESS) {
+
+					@Override
+					protected void onConfigure() {
+						super.onConfigure();
+						setVisible(anyMessage(FeedbackMessage.SUCCESS));
+					}
+
+				};
+		success.setOutputMarkupPlaceholderTag(true);
+		form.add(success);
+
 	}
 
 	private void firstName() {
@@ -94,10 +130,89 @@ public class PersonalInformationPage extends WebPage {
 
 	private void email() {
 
-		EmailTextField email = new EmailTextField(
-				"email",
-				LambdaModel.of(form.getModel(), User::getEmail, User::setEmail));
+		EmailTextField email =
+				new EmailTextField(
+						"email",
+						LambdaModel.of(form.getModel(), User::getEmail, User::setEmail));
 		form.add(email);
+
+	}
+
+	private void country() {
+
+		var country =
+				new DropDownChoice<>(
+						"country",
+						LambdaModel.of(form.getModel(), User::getCountry, User::setCountry),
+						Stream.of(
+										"United States",
+										"Canada",
+										"United Kingdom",
+										"France",
+										"Germany",
+										"Japan",
+										"Italy",
+										"Spain",
+										"Netherlands",
+										"Brazil",
+										"Australia")
+								.sorted()
+								.toList());
+		form.add(country);
+
+	}
+
+	private void streetAddress() {
+
+		var streetAddress =
+				new TextField<>(
+						"streetAddress",
+						LambdaModel.of(form.getModel(), User::getStreetAddress, User::setStreetAddress));
+		form.add(streetAddress);
+
+	}
+
+	private void city() {
+
+		var city = new TextField<>(
+				"city",
+				LambdaModel.of(form.getModel(), User::getCity, User::setCity));
+		form.add(city);
+
+	}
+
+	private void region() {
+
+		var region =
+				new DropDownChoice<>(
+						"region",
+						LambdaModel.of(form.getModel(), User::getRegion, User::setRegion),
+						Stream.of(
+										"Alaska",
+										"Texas",
+										"California",
+										"Montana",
+										"New Mexico",
+										"Arizona",
+										"Nevada",
+										"Colorado",
+										"Oregon",
+										"Ohio",
+										"New Jersey"
+								)
+								.sorted()
+								.toList());
+		form.add(region);
+
+	}
+
+	private void postalCode() {
+
+		var postalCode =
+				new TextField<>(
+						"postalCode",
+						LambdaModel.of(form.getModel(), User::getPostalCode, User::setPostalCode));
+		form.add(postalCode);
 
 	}
 }
